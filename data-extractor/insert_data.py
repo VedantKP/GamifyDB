4d1c91bf108f13de7c75315fb20f0900de705bf0
Vedant Puranik <vedant.puranik@gmail.com>
2023-11-22 11:00:51 +0100

    added createGlobalSchema function for creating db and tables

--- data-extractor/insert_data.py
+++ data-extractor/insert_data.py
@@ -1,5 +1,13 @@
 import psycopg2
 
+dbDetails = {
+    'database': 'postgres',
+    'user': 'postgres',
+    'host': 'localhost',
+    'password': 'TomHanks24$',
+    'port': 5432
+}
+
 def createDb(dbName='gamify_db'):
     conn = psycopg2.connect(database = 'postgres',
                             user = 'postgres',
@@ -14,4 +22,27 @@ def createDb(dbName='gamify_db'):
         print('{} database created!'.format(dbName))
     except Exception as exp:
         print('cannot create database: {}'.format(exp))
+    cursor.close()
+
+def createGlobalSchema(dbName=None,tableQueries=[None]):
+    conn = psycopg2.connect(database = dbDetails['database'],
+                            user = dbDetails['user'],
+                            host = dbDetails['host'],
+                            password = dbDetails['password'],
+                            port = dbDetails['port'])
+    conn.autocommit = True
+    if dbName:
+        createDb(conn,dbName=dbName)
+    else: 
+        print('No database name given!')
+        return
+    
+    if tableQueries[0]:
+        createTables(conn,dbname=dbName,queries=tableQueries)
+    else:
+        print('No table queries given to create tables!')
+        return
+    
+    print('Global Schema created!')
     conn.close()
+    return True
