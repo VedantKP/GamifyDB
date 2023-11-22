2ffa9a4a216fec23994ac20882679a6d99a409eb
Vedant Puranik <vedant.puranik@gmail.com>
2023-11-22 10:30:40 +0100

    added already exists case to db creation

--- data-extractor/insert_data.py
+++ data-extractor/insert_data.py
@@ -4,11 +4,14 @@ def createDb(dbName='gamify_db'):
     conn = psycopg2.connect(database = 'postgres',
                             user = 'postgres',
                             host = 'localhost',
-                            password = '<password>',
+                            password = 'TomHanks24$',
                             port = 5432)
     conn.autocommit = True
     cursor = conn.cursor()
     sql = 'create database {}'.format(dbName)
-    cursor.execute(sql)
-    print('{} database created!'.format(dbName))
+    try:
+        cursor.execute(sql)
+        print('{} database created!'.format(dbName))
+    except Exception as exp:
+        print('cannot create database: {}'.format(exp))
     conn.close()
