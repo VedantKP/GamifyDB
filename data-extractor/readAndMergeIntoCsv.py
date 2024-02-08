98caa4a1d6798a7b77b84610b14b0878417783ec
Vedant Puranik <48618975+VedantKP@users.noreply.github.com>
2024-02-08 15:54:29 +0100

    Update readAndMergeIntoCsv.py
    Final code changes and comments

--- data-extractor/readAndMergeIntoCsv.py
+++ data-extractor/readAndMergeIntoCsv.py
@@ -2,12 +2,11 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-<<<<<<< HEAD
-db_url = 'postgresql://postgres:123@localhost:5432/gamify_db'
-=======
 db_url = 'postgresql://postgres:<password>@localhost:5432/gamify_db'
->>>>>>> b4d21b81fd7011ea1655649f3ebf36caf0eeb8d0
 
+'''
+Function reads all game data and extracts it into a CSV. This CSV is used for creating visualizations
+'''
 if __name__ == "__main__":
     engine = create_engine(db_url)
     df = pd.read_sql_query('select name, rating, votes, year, url, plot, genre, na_sales, eu_sales, jp_sales, other_sales, global_sales, company_name, company_url, company_country, company_startyear from "game" inner join "game_developers" on game.id = game_developers.game_id inner join "company" on game_developers.company_id = company.id',con=engine)
